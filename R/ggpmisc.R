c436f2a92a34016a918900039a02e1e828c6cf23
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2016-10-06 15:57:18 +0300

    Add ggplot2 to Depends and import whole package.

--- R/ggpmisc.R
+++ R/ggpmisc.R
@@ -37,6 +37,9 @@
 #' Package 'ggplot2' documentation at \url{http://docs.ggplot2.org/}\cr
 #' Package 'ggplot2' source code at \url{https://github.com/hadley/ggplot2}
 #'
+#' @import ggplot2
+#' @importFrom ggplot2 ggplot
+#'
 #' @note The signatures of \code{stat_peaks()} and \code{stat_valleys()} are
 #'   identical to those of \code{stat_peaks} and \code{stat_valleys} from
 #'   package \code{photobiology} but the variables returned are a subset as
