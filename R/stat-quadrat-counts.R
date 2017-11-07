459f5d070914e8ad72b7600f0ccb536f62989f55
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2017-11-07 16:48:59 +0200

    Revise stat_quadrat_counts() so that it can optionally count observations in pairs of quadrats.

--- R/stat-quadrat-counts.R
+++ R/stat-quadrat-counts.R
@@ -3,8 +3,8 @@
 #' \code{stat_quadrat_counts} counts the number of observations in each quadrat
 #' of a plot panel. By default it adds a text label to the far corner of each
 #' quadrat. It can also be used to obtain the total number of observations in
-#' the whole panel. Grouping is ignored, so en every case a single count is
-#' computed for each quadrat in a plot panel.
+#' each of two pairs of quadrats or in the whole panel. Grouping is ignored, so
+#' en every case a single count is computed for each quadrat in a plot panel.
 #'
 #' @param mapping The aesthetic mapping, usually constructed with
 #'   \code{\link[ggplot2]{aes}} or \code{\link[ggplot2]{aes_string}}. Only needs
@@ -24,25 +24,26 @@
 #' @param ... other arguments passed on to \code{\link[ggplot2]{layer}}. This
 #'   can include aesthetics whose values you want to set, not map. See
 #'   \code{\link[ggplot2]{layer}} for more details.
-#' @param na.rm	a logical indicating whether NA values should be stripped
-#'   before the computation proceeds.
-#' @param quadrats integer vector indicating which quadrats are of interest, with
-#'   a \code{OL} indicating the whole plot.
-#' @param origin.x,origin.y numeric the coordinates of the origin of the quadrats.
-#' @param labels.range.x,labels.range.y \code{numeric} Coordinates (in data units) to be used
-#'   for absolute positioning of the labels.
+#' @param na.rm	a logical indicating whether NA values should be stripped before
+#'   the computation proceeds.
+#' @param quadrats integer vector indicating which quadrats are of interest,
+#'   with a \code{OL} indicating the whole plot.
+#' @param pool.along character, one of "none", "x" or "y", indicating which
+#'   quadrats to pool to calculate counts by pair of quadrats.
+#' @param origin.x,origin.y numeric the coordinates of the origin of the
+#'   quadrats.
+#' @param labels.range.x,labels.range.y \code{numeric} Coordinates (in data
+#'   units) to be used for absolute positioning of the labels.
 #'
-#' @details This stat can be used to automatically count observations in each of the
-#' four quadrats of a plot, and by default add these counts as text labels.
+#' @details This stat can be used to automatically count observations in each of
+#'   the four quadrats of a plot, and by default add these counts as text
+#'   labels.
 #'
 #' @section Computed variables: Data frame with one to four rows, one for each
-#'   quadrat for which observations are present in \code{data}.
-#'   \describe{
-#'   \item{quadrat}{integer, one of 0:4}
-#'   \item{x}{extreme x value in the quadrat}
-#'   \item{y}{extreme y value in the quadrat}
-#'   \item{count}{number of ovserbations}
-#'   }
+#'   quadrat for which observations are present in \code{data}. \describe{
+#'   \item{quadrat}{integer, one of 0:4} \item{x}{extreme x value in the
+#'   quadrat} \item{y}{extreme y value in the quadrat} \item{count}{number of
+#'   ovserbations} }
 #'
 #' @note Values exactly equal to zero are counted as belonging to the positve
 #'   quadrat. An argument value of zero, passed to formal parameter
@@ -74,6 +75,16 @@
 #'   stat_quadrat_counts(colour = "blue")
 #'
 #' ggplot(my.data, aes(x - 50, y - 10)) +
+#'   geom_hline(yintercept = 0, colour = "blue") +
+#'   geom_point() +
+#'   stat_quadrat_counts(colour = "blue", pool.along = "x")
+#'
+#' ggplot(my.data, aes(x - 50, y - 10)) +
+#'   geom_vline(xintercept = 0, colour = "blue") +
+#'   geom_point() +
+#'   stat_quadrat_counts(colour = "blue", pool.along = "y")
+#'
+#' ggplot(my.data, aes(x - 50, y - 10)) +
 #'   geom_point() +
 #'   stat_quadrat_counts(quadrats = 0)
 #'
@@ -82,6 +93,7 @@
 stat_quadrat_counts <- function(mapping = NULL, data = NULL, geom = "text",
                                 position = "identity",
                                 quadrats = NULL,
+                                pool.along = "none",
                                 origin.x = 0, origin.y = 0,
                                 labels.range.x = NULL, labels.range.y = NULL,
                                 na.rm = FALSE, show.legend = FALSE,
@@ -91,6 +103,7 @@ stat_quadrat_counts <- function(mapping = NULL, data = NULL, geom = "text",
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     params = list(na.rm = na.rm,
                   quadrats = quadrats,
+                  pool.along = pool.along,
                   origin.x = origin.x,
                   origin.y = origin.y,
                   labels.range.x = labels.range.x,
@@ -107,21 +120,29 @@ stat_quadrat_counts <- function(mapping = NULL, data = NULL, geom = "text",
 compute_counts_fun <- function(data,
                                scales,
                                quadrats,
+                               pool.along,
                                origin.x,
                                origin.y,
                                labels.range.x,
                                labels.range.y) {
 
   which_quadrat <- function(x, y) {
-    ifelse(x >= origin.x & y >= origin.y,
-           1L,
-           ifelse(x >= origin.x & y < origin.y,
-                  2L,
-                  ifelse(x < origin.x & y < origin.y,
-                         3L,
-                         4L)))
+    z <- ifelse(x >= origin.x & y >= origin.y,
+                1L,
+                ifelse(x >= origin.x & y < origin.y,
+                       2L,
+                       ifelse(x < origin.x & y < origin.y,
+                              3L,
+                              4L)))
+    if (pool.along == "x") {
+      z <- ifelse(z %in% c(1L, 4L), 1L, 2L)
+    } else if(pool.along == "y") {
+      z <- ifelse(z %in% c(1L, 2L), 1L, 4L)
+    }
+    z
   }
 
+  stopifnot(pool.along %in% c("none", "x", "y"))
   stopifnot(length(origin.x) == 1 && length(origin.y) == 1)
   stopifnot(length(quadrats) <= 4)
   stopifnot(is.null(labels.range.x) || is.numeric(labels.range.x))
@@ -133,12 +154,20 @@ compute_counts_fun <- function(data,
   range.y <- range(data$y)
   # compute postion for labels
   if (is.null(labels.range.x)) {
+    if (pool.along == "x") {
+      labels.range.x <- rep(origin.x, 2)
+    } else {
     labels.range.x <- range.x
+    }
   } else {
     labels.range.x <- range(labels.range.x)
   }
   if (is.null(labels.range.y)) {
-    labels.range.y <- range.y
+    if (pool.along == "y") {
+      labels.range.y <- rep(origin.y, 2)
+    } else {
+      labels.range.y <- range.y
+    }
   } else {
     labels.range.y <- range(labels.range.y)
   }
@@ -156,6 +185,12 @@ compute_counts_fun <- function(data,
     } else {
       quadrats = c(1L, 2L, 3L, 4L)
     }
+  }
+  if (pool.along == "x") {
+    quadrats <- intersect(quadrats, c(1L, 2L))
+  }
+  if (pool.along == "y") {
+    quadrats <- intersect(quadrats, c(1L, 4L))
   }
 
   if (all(is.na(quadrats)) || 0L %in% quadrats) {
@@ -176,6 +211,7 @@ compute_counts_fun <- function(data,
       dplyr::ungroup() -> data
 
     zero.count.quadrats <- setdiff(quadrats, data$quadrat)
+
     if (length(zero.count.quadrats) > 0) {
       data <-
         rbind(data, tibble::tibble(quadrat = zero.count.quadrats, count = 0L))
@@ -190,7 +226,7 @@ compute_counts_fun <- function(data,
                                labels.range.y[1]),
                     hjust = ifelse(.data$quadrat %in% c(1L, 2L), 1, 0),
                     vjust = ifelse(.data$quadrat %in% c(1L, 4L), -0.1, 1.1))
-  }
+   }
 }
 
 #' @rdname ggpmisc-ggproto
