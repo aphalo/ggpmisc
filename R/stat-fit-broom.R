9026eb4ee640fd0f7c53c98671f584a19231d6d5
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2018-05-03 17:33:59 +0300

    Remove commented out code statement.

--- R/stat-fit-broom.R
+++ R/stat-fit-broom.R
@@ -433,7 +433,6 @@ fit_tidy_compute_group_fun <- function(data,
   z.std.error <- as.data.frame(t(mf.td[["std.error"]]))
   clean.term.names <- gsub("(Intercept)", "Intercept", mf.td[["term"]], fixed = TRUE)
   names(z.estimate) <- paste(clean.term.names, "estimate", sep = "_")
-  #    names(z.estimate) <- clean.term.names
   names(z.std.error) <- paste(clean.term.names, "se", sep = "_")
   z <- cbind(z.estimate, z.std.error)
   if (exists("statistic", mf.td, inherits = FALSE)) {
