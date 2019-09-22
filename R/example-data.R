9ebb4b817eb1037ffb88686752adeb986413d6b6
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2019-09-22 22:34:20 +0300

    Add genomics example data
    As 'ggomics' seems to be a dead end and a bad idea to have such hardwired functions I moved the data to 'ggpmisc'.

---
+++ R/example-data.R
@@ -0,0 +1,32 @@
+#' @title Example gene expression data
+#'
+#' @description A dataset containing reshaped and simplified output from an
+#'   analysis of data from RNAseq done with package edgeR.
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A \code{data.frame} object with 1218 rows and 5 variables
+#' @family Spectral data examples
+#'
+#' @examples
+#' names(volcano_example.df)
+#' head(volcano_example.df)
+#'
+"volcano_example.df"
+
+#' @title Example gene expression data
+#'
+#' @description A dataset containing reshaped and simplified output from an
+#'   analysis of data from RNAseq done with package edgeR.
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A \code{data.frame} object with 6088 rows and 6 variables
+#' @family Spectral data examples
+#'
+#' @examples
+#' names(quadrant_example.df)
+#' head(quadrant_example.df)
+#'
+"quadrant_example.df"
+
