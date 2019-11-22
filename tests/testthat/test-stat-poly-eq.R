73eb649bf57dff590f5ffff9417a7d23ebe2c67f
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2019-11-23 01:51:16 +0200

    Add test cases for stat_poly_eq()

---
+++ tests/testthat/test-stat-poly-eq.R
@@ -0,0 +1,151 @@
+context("stat_poly_eq")
+
+library(tibble)
+
+set.seed(4321)
+# generate artificial data
+x <- 1:100
+y <- (x + x^2 + x^3) + rnorm(length(x), mean = 0, sd = mean(x^3) / 4)
+my.data <- data.frame(x,
+                      y,
+                      group = c("A", "B"),
+                      y2 = y * c(0.5,2),
+                      block = c("a", "a", "b", "b"),
+                      wt = sqrt(x))
+                      formula <- y ~ poly(x, 3, raw = TRUE)
+
+test_that("textual_positions", {
+  vdiffr::expect_doppelganger("stat_poly_eq_0",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE)
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_1",
+                               ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             geom = "text_npc")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_2",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             geom = "label_npc")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_3",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             geom = "text")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_4",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             geom = "label")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_5",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = "right", label.y = "bottom",
+                                             geom = "text_npc")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_6",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = "right", label.y = "bottom",
+                                             geom = "label_npc")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_7",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = "right", label.y = "bottom",
+                                             geom = "text")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_8",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = "right", label.y = "bottom",
+                                             geom = "label")
+  )
+})
+
+test_that("numric_positions", {
+  vdiffr::expect_doppelganger("stat_poly_eq_n1",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = 0, label.y = 0,
+                                             geom = "text_npc")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_n2",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = 0, label.y = 0,
+                                             geom = "label_npc")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_n3",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = 0, label.y = -1e5,
+                                             geom = "text")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_n4",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = 0, label.y = -1e5,
+                                             geom = "label")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_n5",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = 1, label.y = 0.5,
+                                             geom = "text_npc")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_n6",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = 1, label.y = 0.5,
+                                             geom = "label_npc")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_n7",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = 100, label.y = 5e5,
+                                             geom = "text")
+  )
+  vdiffr::expect_doppelganger("stat_poly_eq_n8",
+                              ggplot(my.data, aes(x, y)) +
+                                geom_point() +
+                                stat_smooth(method = "lm", formula = formula) +
+                                stat_poly_eq(formula = formula, parse = TRUE,
+                                             label.x = 100, label.y = 5e5,
+                                             geom = "label")
+  )
+})
