c50cfa6d7a763b1518ea2c9bd1659a71bbc31898
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2023-02-24 14:04:22 +0200

    Disable some tests under unix like
    The visual output differs from that under Windows

--- tests/testthat/test-stat-corr.R
+++ tests/testthat/test-stat-corr.R
@@ -14,32 +14,36 @@ if (isNamespaceLoaded(name = "package:ggplot2")) detach(package:ggplot2, unload 
 
 test_that("corr_noload", {
   set.seed(4321)
-  vdiffr::expect_doppelganger("stat_correlation_pearson_noload",
-                              ggplot2::ggplot(my.data, ggplot2::aes(x, y)) +
-                                ggplot2::geom_point() +
-                                ggpmisc::stat_correlation(method = "pearson",
-                                                          mapping =
-                                                            ggplot2::aes(label = paste(ggplot2::after_stat(r.label),
-                                                                                       ggplot2::after_stat(r.confint.label),
-                                                                                       ggplot2::after_stat(t.value.label),
-                                                                                       ggplot2::after_stat(p.value.label),
-                                                                                       ggplot2::after_stat(n.label),
-                                                                                       sep = "~~")))
-  )
-  vdiffr::expect_doppelganger("stat_correlation_pearson_noload_use_label",
-                              ggplot2::ggplot(my.data, ggplot2::aes(x, y)) +
-                                ggplot2::geom_point() +
-                                ggpmisc::stat_correlation(method = "pearson",
-                                                          mapping = ggpmisc::use_label(c("R", "R.CI", "t", "P", "n"),
-                                                                                       sep = "~~"))
-  )
-  vdiffr::expect_doppelganger("stat_correlation_pearson_noload_use_label_lc",
-                              ggplot2::ggplot(my.data, ggplot2::aes(x, y)) +
-                                ggplot2::geom_point() +
-                                ggpmisc::stat_correlation(method = "pearson",
-                                                          mapping = ggpmisc::use_label(c("cor", "r.ci", "t", "p", "n"),
-                                                                                       sep = "~~"))
-  )
+  { # output under linux does not match snapshots created under Windows
+    skip_on_os(c("mac", "linux", "solaris"))
+
+    vdiffr::expect_doppelganger("stat_correlation_pearson_noload",
+                                ggplot2::ggplot(my.data, ggplot2::aes(x, y)) +
+                                  ggplot2::geom_point() +
+                                  ggpmisc::stat_correlation(method = "pearson",
+                                                            mapping =
+                                                              ggplot2::aes(label = paste(ggplot2::after_stat(r.label),
+                                                                                         ggplot2::after_stat(r.confint.label),
+                                                                                         ggplot2::after_stat(t.value.label),
+                                                                                         ggplot2::after_stat(p.value.label),
+                                                                                         ggplot2::after_stat(n.label),
+                                                                                         sep = "~~")))
+    )
+    vdiffr::expect_doppelganger("stat_correlation_pearson_noload_use_label",
+                                ggplot2::ggplot(my.data, ggplot2::aes(x, y)) +
+                                  ggplot2::geom_point() +
+                                  ggpmisc::stat_correlation(method = "pearson",
+                                                            mapping = ggpmisc::use_label(c("R", "R.CI", "t", "P", "n"),
+                                                                                         sep = "~~"))
+    )
+    vdiffr::expect_doppelganger("stat_correlation_pearson_noload_use_label_lc",
+                                ggplot2::ggplot(my.data, ggplot2::aes(x, y)) +
+                                  ggplot2::geom_point() +
+                                  ggpmisc::stat_correlation(method = "pearson",
+                                                            mapping = ggpmisc::use_label(c("cor", "r.ci", "t", "p", "n"),
+                                                                                         sep = "~~"))
+    )
+  }
   vdiffr::expect_doppelganger("stat_correlation_kendall_noload",
                               ggplot2::ggplot(my.data, ggplot2::aes(x, y)) +
                                 ggplot2::geom_point() +
@@ -99,18 +103,21 @@ library(ggpmisc)
 
 test_that("corr_load", {
   set.seed(4321)
-  vdiffr::expect_doppelganger("stat_correlation_pearson_load",
-                              ggplot(my.data, aes(x, y)) +
-                                geom_point() +
-                                stat_correlation(method = "pearson",
-                                                 mapping =
-                                                   aes(label = paste(after_stat(r.label),
-                                                                     after_stat(r.confint.label),
-                                                                     after_stat(t.value.label),
-                                                                     after_stat(p.value.label),
-                                                                     after_stat(n.label),
-                                                                     sep = "~~")))
-  )
+  { # output under linux does not match snapshots created under Windows
+    skip_on_os(c("mac", "linux", "solaris"))
+    vdiffr::expect_doppelganger("stat_correlation_pearson_load",
+                                ggplot(my.data, aes(x, y)) +
+                                  geom_point() +
+                                  stat_correlation(method = "pearson",
+                                                   mapping =
+                                                     aes(label = paste(after_stat(r.label),
+                                                                       after_stat(r.confint.label),
+                                                                       after_stat(t.value.label),
+                                                                       after_stat(p.value.label),
+                                                                       after_stat(n.label),
+                                                                       sep = "~~")))
+    )
+  }
   vdiffr::expect_doppelganger("stat_correlation_kendall_load",
                               ggplot(my.data, aes(x, y)) +
                                 geom_point() +
@@ -135,49 +142,54 @@ test_that("corr_load", {
                                                                      after_stat(n.label),
                                                                      sep = "~~")))
   )
-  vdiffr::expect_doppelganger("stat_correlation_pearson_small",
-                              ggplot(my.data, aes(x, y)) +
-                                geom_point() +
-                                stat_correlation(method = "pearson", small.r = TRUE, small.p = TRUE,
-                                                 mapping =
-                                                   aes(label = paste(after_stat(r.label),
-                                                                     after_stat(r.confint.label),
-                                                                     after_stat(t.value.label),
-                                                                     after_stat(p.value.label),
-                                                                     after_stat(n.label),
-                                                                     sep = "~~")))
-  )
+  { # output under linux does not match snapshots created under Windows
+    skip_on_os(c("mac", "linux", "solaris"))
+    vdiffr::expect_doppelganger("stat_correlation_pearson_small",
+                                ggplot(my.data, aes(x, y)) +
+                                  geom_point() +
+                                  stat_correlation(method = "pearson", small.r = TRUE, small.p = TRUE,
+                                                   mapping =
+                                                     aes(label = paste(after_stat(r.label),
+                                                                       after_stat(r.confint.label),
+                                                                       after_stat(t.value.label),
+                                                                       after_stat(p.value.label),
+                                                                       after_stat(n.label),
+                                                                       sep = "~~")))
+    )
+  }
 })
 
 test_that("corr_exact", {
   set.seed(4321)
-  vdiffr::expect_doppelganger("stat_correlation_pearson_exact",
-                              ggplot(my.data, aes(x, y)) +
-                                geom_point() +
-                                stat_correlation(method = "pearson", exact = TRUE,
-                                                 mapping =
-                                                   aes(label = paste(after_stat(r.label),
-                                                                     after_stat(r.confint.label),
-                                                                     after_stat(t.value.label),
-                                                                     after_stat(p.value.label),
-                                                                     after_stat(n.label),
-                                                                     sep = "~~")))
-  )
+  { # output under linux does not match snapshots created under Windows
+    skip_on_os(c("mac", "linux", "solaris"))
+    vdiffr::expect_doppelganger("stat_correlation_pearson_exact",
+                                ggplot(my.data, aes(x, y)) +
+                                  geom_point() +
+                                  stat_correlation(method = "pearson", exact = TRUE,
+                                                   mapping =
+                                                     aes(label = paste(after_stat(r.label),
+                                                                       after_stat(r.confint.label),
+                                                                       after_stat(t.value.label),
+                                                                       after_stat(p.value.label),
+                                                                       after_stat(n.label),
+                                                                       sep = "~~")))
+    )
 
-  vdiffr::expect_doppelganger("stat_correlation_pearson_exact_boot",
-                              ggplot(my.data, aes(x, y)) +
-                                geom_point() +
-                                stat_correlation(method = "pearson", exact = TRUE,
-                                                 boot.R = 999,
-                                                 mapping =
-                                                   aes(label = paste(after_stat(r.label),
-                                                                     after_stat(r.confint.label),
-                                                                     after_stat(t.value.label),
-                                                                     after_stat(p.value.label),
-                                                                     after_stat(n.label),
-                                                                     sep = "~~")))
-  )
-
+    vdiffr::expect_doppelganger("stat_correlation_pearson_exact_boot",
+                                ggplot(my.data, aes(x, y)) +
+                                  geom_point() +
+                                  stat_correlation(method = "pearson", exact = TRUE,
+                                                   boot.R = 999,
+                                                   mapping =
+                                                     aes(label = paste(after_stat(r.label),
+                                                                       after_stat(r.confint.label),
+                                                                       after_stat(t.value.label),
+                                                                       after_stat(p.value.label),
+                                                                       after_stat(n.label),
+                                                                       sep = "~~")))
+    )
+  }
   vdiffr::expect_doppelganger("stat_correlation_kendall_exact",
                               ggplot(my.data, aes(x, y)) +
                                 geom_point() +
@@ -208,19 +220,21 @@ test_that("corr_exact", {
 
 test_that("corr_continuity", {
   set.seed(4321)
-  vdiffr::expect_doppelganger("stat_correlation_pearson_cont",
-                              ggplot(my.data, aes(x, y)) +
-                                geom_point() +
-                                stat_correlation(method = "pearson", continuity = TRUE,
-                                                 mapping =
-                                                   aes(label = paste(after_stat(r.label),
-                                                                     after_stat(r.confint.label),
-                                                                     after_stat(t.value.label),
-                                                                     after_stat(p.value.label),
-                                                                     after_stat(n.label),
-                                                                     sep = "~~")))
-  )
-
+  { # output under linux does not match snapshots created under Windows
+    skip_on_os(c("mac", "linux", "solaris"))
+    vdiffr::expect_doppelganger("stat_correlation_pearson_cont",
+                                ggplot(my.data, aes(x, y)) +
+                                  geom_point() +
+                                  stat_correlation(method = "pearson", continuity = TRUE,
+                                                   mapping =
+                                                     aes(label = paste(after_stat(r.label),
+                                                                       after_stat(r.confint.label),
+                                                                       after_stat(t.value.label),
+                                                                       after_stat(p.value.label),
+                                                                       after_stat(n.label),
+                                                                       sep = "~~")))
+    )
+  }
   vdiffr::expect_doppelganger("stat_correlation_kendall_cont",
                               ggplot(my.data, aes(x, y)) +
                                 geom_point() +
