e1120ae1b5b7c30b117df07fd678a4b76dfb6b8d
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2023-08-17 01:59:42 +0300

    Update ggpmisc-package docs
    Was very outdated

--- R/ggpmisc.R
+++ R/ggpmisc.R
@@ -1,9 +1,9 @@
 #' @details The new facilities for cleanly defining new stats and geoms added to
 #' 'ggplot2' in version 2.0.0 and the support for nested tibbles and new syntax
 #' for mapping computed values to aesthetics added to
-#' 'ggplot2' in version 3.0.0 are used in this package's code. This
-#' means that 'ggpmisc' (>= 0.3.0) requires version 3.0.0 or later of ggplot2
-#' while 'ggpmisc' (< 0.3.0) requires version 2.0.0 or later of ggplot2.
+#' 'ggplot2' in version 3.0.0 are used in this package's code, as well as some
+#' features added in more recent updates. This
+#' means that current 'ggpmisc' versions require recent versions of ggplot2.
 #'
 #' Extensions provided:
 #' \itemize{
@@ -12,32 +12,17 @@
 #' \item \code{ggplot()} method for time series data.
 #' \item Stats for locating and tagging "peaks" and "valleys" (local or global
 #'  maxima and minima).
-#' \item Stat for generating labels from a \code{lm()} model fit, including
-#' formatted equation. By default labels are expressions but tikz device is
-#' supported optionally with LaTeX formatted labels.
+#' \item Stat for generating labels from model fit objects, including
+#' formatted equations. By default labels are R's plotmath expressions but LaTeX, markdown
+#' and plain text formatted labels are optionaly assembled.
 #' \item Stats for extracting information from a any model fit supported by
-#' package 'broom'.
-#' \item Stats for filtering-out/filtering-in observations in regions of a
-#' panel or group where the density of observations is high.
-#' \item Geom for annotating plots with tables.
-#' }
+#' package 'broom' and using it to generate various annotations and data labels.
+#' \item Stat for computing and generating labels for the results from multiple
+#' comparisons, including adjusted \emph{P}-values.
 #'
 #' The stats for peaks and valleys are coded so as to work correctly both with
 #' numeric and POSIXct variables mapped to the x aesthetic. Special handling was
 #' needed as text labels are generated from the data.
-#'
-#' @section Warning!: \code{geom_null()}, \code{stat_debug_group()},
-#'   \code{stat_debug_panel()}, \code{geom_debug()}, \code{append_layers()},
-#'   \code{bottom_layer()}, \code{delete_layers()}, \code{extract_layers()},
-#'   \code{move_layers()}, \code{num_layesr()}, \code{shift_layers()},
-#'   \code{top_layer()} and \code{which_layers()} have been moved from package
-#'   'ggpmisc' into their own separate package
-#'   '\code{\link[gginnards]{gginnards-package}}.
-#'
-#' @section Acknowledgements: We thank Kamil Slowikowski not only for
-#'   contributing ideas and code examples to this package but also for adding
-#'   new features to his package 'ggrepel' that allow new use cases for
-#'   \code{stat_dens2d_labels} from this package.
 #'
 #' @references Package suite 'r4photobiology' web site at
 #' \url{https://www.r4photobiology.info/}\cr Package 'ggplot2' documentation at
@@ -48,13 +33,14 @@
 #' @importFrom rlang .data
 #' @importFrom ggplot2 expansion after_stat
 #'
-#' @note The signatures of \code{stat_peaks()} and \code{stat_valleys()} are
-#'   identical to those of \code{stat_peaks} and \code{stat_valleys} from
-#'   package \code{photobiology} but the variables returned are a subset as
-#'   values related to light spectra are missing. Furthermore the stats from
-#'   package \code{ggpmisc} work correctly when the x aesthetic uses a date or
-#'   datetime scale, while those from package \code{photobiology} do not
-#'   generate correct labels in this case.
+#' @note The signatures of \code{stat_peaks()} and \code{stat_valleys()} from
+#'   'ggpmisc' are identical to those of \code{stat_peaks} and
+#'   \code{stat_valleys} from package 'ggspectra' but the variables returned are
+#'   a subset as special handling of values related to light spectra is missing.
+#'   Furthermore the \code{stat_peaks()} and \code{stat_valleys()} from package
+#'   'ggpmisc' work correctly when date or datetime values are mapped to the
+#'   \emph{x} statistic, while those from package 'ggspectra' do not generate
+#'   correct labels in this case.
 #'
 #' @examples
 #' library(tibble)
